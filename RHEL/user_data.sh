949045b230754a56ec2a75ace0181243567e2b31
Sandipan Ghosh <sandipanghosh@Sandipans-MacBook-Air.local>
2024-01-27 18:17:41 +0530

    new-status38

--- RHEL/user_data.sh
+++ RHEL/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo unzip awscliv2.zip
-sudo ./aws/install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
 sudo hostnamectl set-hostname test-ami
